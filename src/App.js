c6a08723cd2447f09499e7be66bccb0ec319fbd0
Gustavo <104770923+12Gustavo21@users.noreply.github.com>
2022-07-12 09:53:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import  React, { Component } from "react";
+import "./styles.css"
+
+class App extends Component{
+  render(){
+    return(
+      <section>
+        <h1>LOL</h1>
+        <h2>LMAO</h2>
+        <p>🤨🤨🤨</p>
+      </section>
+    )
+  }
+}
+export default App;
